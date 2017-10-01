6a58e0998fe19dfb734465945839249da1ef26de
sky_k <sky_karlos@hotmail.com>
2017-09-30 20:08:03 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {Ng4FittextModule} from 'ng4-fittext';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Ng4FittextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
