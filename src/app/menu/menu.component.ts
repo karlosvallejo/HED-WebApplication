1c53432d724e1b9901dee41b9783088cca240307
sky_k <sky_karlos@hotmail.com>
2017-09-30 23:52:26 -0500

    menu - maquetacion

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,16 +1,23 @@
 import {Component, OnInit} from '@angular/core';
-import {GeneralServices} from "../services/services.service";
+//import {GeneralServices} from "../services/services.service";
+//providers: [GeneralServices]
+import {Ng4FittextModule} from 'ng4-fittext';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css'], providers: [GeneralServices]
+  styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
   identidad_data: any;
 
-  constructor(private services: GeneralServices) {
+/*  constructor(private services: GeneralServices) {
     this.identidad_data = this.services.data_identidad;
+  }
+  */
+
+  constructor() {
+
   }
 
   ngOnInit() {
