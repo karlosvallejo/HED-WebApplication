2e649e5fee14d5dc5acefa52afc0d69d502f8e7a
sky_k <sky_karlos@hotmail.com>
2017-10-01 20:39:32 -0500

    about - maquetacion

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, ElementRef, ViewChild, AfterViewInit} from '@angular/core';
-import {trigger, state, style, transition, animate} from "@angular/animations";
+import {trigger, state, style, transition, animate} from '@angular/animations';
 
 @Component({
   selector: 'app-about',
@@ -10,15 +10,27 @@ export class AboutComponent implements OnInit, AfterViewInit {
   @ViewChild('containerSelector', {read: ElementRef})
   containerSelector: ElementRef;
 
-info: string;
+infoQUE: string;
+infoQUIEN: string;
 maxScrollLeft: number;
+selectedSection: string;
+seletedInfo: string;
+
 
   constructor() {
+    this.selectedSection = 'QUE';
 
-    this.info = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ' +
+    this.infoQUE = 'QUE Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ' +
       'dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea' +
       ' commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ' +
       'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+    this.infoQUIEN = 'QUien Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ' +
+      'dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea' +
+      ' commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ' +
+      'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+    this.seletedInfo = this.infoQUE;
   }
 
   ngOnInit() {
@@ -27,6 +39,21 @@ maxScrollLeft: number;
 
   ngAfterViewInit() {
     this.maxScrollLeft = this.containerSelector.nativeElement.scrollWidth - this.containerSelector.nativeElement.clientWidth;
+  }
+
+
+  public asignarInfo(selected: string){
+    switch (selected) {
+      case 'QUE':
+        this.selectedSection = 'QUE';
+        this.seletedInfo = this.infoQUE;
+        break;
+
+      case 'QUIEN':
+        this.selectedSection= 'QUIEN';
+        this.seletedInfo = this.infoQUIEN;
+        break;
+    }
   }
 
   public onNextSearchPosition(objetivo: number): void {
@@ -40,7 +67,7 @@ maxScrollLeft: number;
         if (i > this.maxScrollLeft - 15) {
           clearInterval(init);
         }
-      }, 10);
+      }, 5);
     } else if (this.containerSelector.nativeElement.scrollLeft > objetivo) {
       let i = this.containerSelector.nativeElement.scrollLeft;
       const init = setInterval(() => {
@@ -49,7 +76,7 @@ maxScrollLeft: number;
         if (i < objetivo + 15) {
           clearInterval(init);
         }
-      }, 10);
+      }, 5);
     }
     console.log(this.containerSelector.nativeElement.scrollLeft + ' Scroll total: ' + this.maxScrollLeft);
   //  this.containerSelector.nativeElement.scrollLeft = 350;
