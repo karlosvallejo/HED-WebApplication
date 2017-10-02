14f55079bb8e3c41ebe76266b99b7508f5c1d6d1
sky_k <sky_karlos@hotmail.com>
2017-10-01 20:08:42 -0500

    about - maquetacion

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,13 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ElementRef, ViewChild, AfterViewInit} from '@angular/core';
+import {trigger, state, style, transition, animate} from "@angular/animations";
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements OnInit, AfterViewInit {
+  @ViewChild('containerSelector', {read: ElementRef})
+  containerSelector: ElementRef;
+
 info: string;
+maxScrollLeft: number;
+
   constructor() {
+
     this.info = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ' +
       'dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea' +
       ' commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ' +
@@ -15,6 +22,40 @@ info: string;
   }
 
   ngOnInit() {
+
   }
+
+  ngAfterViewInit() {
+    this.maxScrollLeft = this.containerSelector.nativeElement.scrollWidth - this.containerSelector.nativeElement.clientWidth;
+  }
+
+  public onNextSearchPosition(objetivo: number): void {
+
+    this.maxScrollLeft = this.containerSelector.nativeElement.scrollWidth - this.containerSelector.nativeElement.clientWidth;
+    if (this.containerSelector.nativeElement.scrollLeft < objetivo) {
+      let i = this.containerSelector.nativeElement.scrollLeft;
+      const init = setInterval(() => {
+        this.containerSelector.nativeElement.scrollTo(i, 0);
+        i += 5;
+        if (i > this.maxScrollLeft - 15) {
+          clearInterval(init);
+        }
+      }, 10);
+    } else if (this.containerSelector.nativeElement.scrollLeft > objetivo) {
+      let i = this.containerSelector.nativeElement.scrollLeft;
+      const init = setInterval(() => {
+        this.containerSelector.nativeElement.scrollTo(i, 0);
+        i -= 5;
+        if (i < objetivo + 15) {
+          clearInterval(init);
+        }
+      }, 10);
+    }
+    console.log(this.containerSelector.nativeElement.scrollLeft + ' Scroll total: ' + this.maxScrollLeft);
+  //  this.containerSelector.nativeElement.scrollLeft = 350;
+
+   // this.containerSelector.nativeElement.scrollTo(350, 0);
+  }
+
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HomeComponent } from './home/home.component';
 import {routes} from './app.routes';
 import { PonentesComponent } from './ponentes/ponentes.component';
 import { AboutComponent } from './about/about.component';
+import { AgendaComponent } from './agenda/agenda.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { AboutComponent } from './about/about.component';
     FooterComponent,
     HomeComponent,
     PonentesComponent,
-    AboutComponent
+    AboutComponent,
+    AgendaComponent
   ],
   imports: [
     routes,
