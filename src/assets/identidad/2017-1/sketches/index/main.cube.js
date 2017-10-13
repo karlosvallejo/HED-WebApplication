0ed2b6e3a496ed7595ca2b602e4b79cfed1bbc61
Andrés Gc <andres01gc@hotmail.com>
2017-10-13 10:26:37 -0500

    fondo animado works

---
+++ src/assets/identidad/2017-1/sketches/index/main.cube.js
@@ -0,0 +1,46 @@
+function Cube() {
+
+  this.pos = createVector(Math.random() * windowWidth, Math.random() * windowHeight); // single bar width
+  this.acel = createVector(0, -Math.random()); // single bar width
+  this.vel = createVector(0, 0); // single bar width
+  // this.imgCube = null;
+
+  //el mismo reset
+  this.imgCube = loadImage("assets/identidad/2017-1/sketches/index/cube.png");  // Load the image
+  this.tam = 20 + Math.random() * 50;
+  this.imgCube.resize(this.tam, this.tam);
+  this.acel = createVector(0, -Math.random()); // single bar width
+
+
+  this.move = function () {
+    this.pos.add(this.vel);
+    this.vel.limit(2 / this.tam);
+    this.vel.add(this.acel);
+  };
+
+  this.display = function () {
+
+    this.imgCube.resize(this.tam, this.tam);
+    image(this.imgCube, this.pos.x, this.pos.y);
+    fill(255);
+    // rect(this.pos.x, this.pos.y, this.tam, this.tam);
+    this.move();
+    this.limites();
+  };
+
+  this.limites = function () {
+    if (this.pos.y < -this.tam) {
+      this.pos.y = windowHeight + this.tam;
+      this.pos.x = Math.random() * windowWidth;
+      this.reset();
+    }
+  };
+
+  this.reset = function () {
+    this.imgCube = loadImage("assets/identidad/2017-1/sketches/index/cube.png");  // Load the image
+    this.tam = 20 + Math.random() * 50;
+    this.imgCube.resize(this.tam, this.tam);
+    this.acel = createVector(0, -Math.random()); // single bar width
+  };
+
+}
