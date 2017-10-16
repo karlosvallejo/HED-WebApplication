71355fadc71bdb2b338b943928550a481aae0ed9
sky_k <sky_karlos@hotmail.com>
2017-10-16 15:30:15 -0500

    pequeños cambios en firebase

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,17 +1,16 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {GeneralServices} from '../services/services.service';
 
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css'], providers: [GeneralServices]
+  styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
  @Input() identidad_data_Menu: any;
 
 
-  constructor(private services: GeneralServices) {
+  constructor() {
   }
 
   ngOnInit() {
