d34ec43ec4e979ec093764542b5c84d0ffb5fb19
sky_k <sky_karlos@hotmail.com>
2017-11-10 12:00:04 -0500

    ediciones

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {GeneralServices} from '../services/services.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,11 +8,20 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class FooterComponent implements OnInit {
  @Input() general_info_footer: any;
+ eventoActual: string;
 
-  constructor() {
+  constructor(private services: GeneralServices) {
   }
 
   ngOnInit() {
+    this.eventoActual = this.services.evento_actual;
+  }
+
+  openUrl(url: string) {
+    console.log(url);
+    if (url === '2017-1') {
+      window.open('http://papelypixel.hoyesdiseno.com/');
+    }
 
   }
 
